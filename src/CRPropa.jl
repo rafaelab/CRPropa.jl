16e6e9c9da19a336b87cfa7e8fc1e06cb26204ae
Rafael <rafaelab@users.noreply.github.com>
2024-11-16 17:27:52 +0100

    document code and organise it

--- src/CRPropa.jl
+++ src/CRPropa.jl
@@ -8,11 +8,13 @@ import PythonCall:
 	pycopy!
 
 
-
+# ------------------------------------------------------------------------------------------------#
+#
 # define and export `crpropa` module
 const crpropa = pynew()
+export crpropa
 
-
+# these items are the objects that we want to link to the CRPropa module
 items = (
 	:Candidate, 
 	
@@ -32,24 +34,45 @@ items = (
 	)
 
 
-# Macro to define constants with pynew
+# ------------------------------------------------------------------------------------------------#
+#
+@doc"""
+Define a constant object with pynew.
+This is a macro because we need the object to be at top level.
+It creates, in reality, a pointer to the Python object, which is then linked to the actual CRPropa object.
+"""
 macro defineObjects(name)
 	quote
 		const $(esc(name)) = pynew()
 	end
 end
 
+# define the objects
 for item in items
 	@eval @defineObjects $(item)
 end
 
 
+# ------------------------------------------------------------------------------------------------#
+#
+@doc"""
+Link the objects to the CRPropa module.
+This allows us to use the objects as if they were defined in Julia.
+"""
 macro linkObjects(name)
 	quote
 		pycopy!($(esc(name)), crpropa.$(Symbol("$name")))
 	end
 end
 
+
+# ------------------------------------------------------------------------------------------------#
+#
+@doc"""
+This function is not exported.
+It is called when loading the module.
+It is essential to handle the PythonCall objects.
+"""
 function __init__()
     pycopy!(crpropa, pyimport("crpropa"))
 
@@ -59,6 +82,9 @@ function __init__()
 end
 
 
+
+# ------------------------------------------------------------------------------------------------#
+#
 # export 
 # 	Candidate,
 # 	EeV
